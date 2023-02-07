f72cda9cca29e1e33e8703896b93d13b856d4f08
stellakeany <stella.keany@ucdconnect.ie>
2023-02-07 17:03:08 +0000

    Implemented basic layout for cart page
    Created basic layout for the cart page, should be responsive for all screen types
    
    Page contains a title and two columns below (stacked if on mobile)
    The column on the left will contain the cart items and the column on the right will contain total, proceed to checkout button etc.
    
    Will change colours to match design in wireframe later but right now I have them in different colours for clarity

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,5 +1,13 @@
 export default function Cart() {
   return (
-    <div>Cart</div>
+    <div className="h-screen md:h-[80vh] w-full">
+      <div className="bg-bglight h-1/5 pl-5 md:pl-10 pt-10">
+        <h2 className="font text-2xl font-bold tracking-tight text-alt sm:text-4xl">My Cart</h2>
+      </div>
+      <div className="bg-alt h-[75vh] grid grid-cols-1 md:grid-cols-5">
+        <div className="bg-bglight h-full col-span-3"> column 1</div>
+        <div className="bg-bgdark h-full col-span-2">column 2</div>
+      </div>
+    </div>
   )
 }
