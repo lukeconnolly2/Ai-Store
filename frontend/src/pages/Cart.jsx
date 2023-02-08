3a158a63ebde286df63a372e3182916ff992fe10
Luke Connolly <luke.connolly2@ucdconnect.ie>
2023-02-08 16:22:02 +0000

    Removed animation from Cart page untill its complete

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,14 +1,6 @@
 import {motion} from "framer-motion"
 export default function Cart() {
   return (
-    <motion.main
-    className="main__container"
-    initial={{ opacity: 0 }}
-     animate={{ opacity: 100 }}
-     exit={{ opacity: 0 }}
-     transition={{ duration: .2 }}
-    >
       <div>Cart</div>
-    </motion.main>
   )
 }
