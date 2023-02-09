5f9a0144d338c2bc6ac5ec11dad13ec7f7f72ba6
Luke Connolly <luke.connolly2@ucdconnect.ie>
2023-02-09 09:49:17 +0000

    Added Shop context

---
+++ frontend/src/context/ShopContext.jsx
@@ -0,0 +1,18 @@
+import {createContext, useState} from "react"
+import { TESTPRODUCTS } from "../../TESTPRODUCTSDISPLAY"
+
+export const ShopContext = createContext(null)
+
+const getDefaultCart = () => {
+    let cart = {}
+    for(let i = 1; i < TESTPRODUCTS.length + 1; i++ ){
+        cart[i] = {}
+    }
+}
+
+export const ShopContextProvider = (props) => {
+    const [cartItems, setCartItems] = useState()
+    return(
+        <ShopContextProvider> {props.children}</ShopContextProvider>
+    )
+}
