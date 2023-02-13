09c6cc2440f1e1df6d1ca339ec0bfb07f08d094b
stellakeany <stella.keany@ucdconnect.ie>
2023-02-13 16:17:52 +0000

    Empty cart rendering
    Made it so that if the cart is empty, the user is not allowed to proceed to checkout and is instead redirected to products page

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -14,17 +14,25 @@ export default function Cart() {
       </div>
       <div className="h-5/6 grid grid-cols-1 md:grid-cols-5 gap-5">
         <div className="h-full col-span-3 outline outline-offset-0 outline-3 p-5 outline-primary rounded"> {
-          TESTPRODUCTSALL.filter((prod) => (cartItems[prod.id] > 0)).map((prod) => (
-            <ProductPreview product={prod} />
-          ))
+          cartTotal() === 0? 
+            <p className="font text-9xl text-center">:(</p> :
+            TESTPRODUCTSALL.filter((prod) => (cartItems[prod.id] > 0)).map((prod) => (
+              <ProductPreview product={prod} />
+            ))
         } </div>
         <div className="h-full col-span-2 p-5">
           <div className="w-full h-5/6">
-          Total is
-              <div className="font text-2xl text-center">€{cartTotal()}</div>
+            <div> {
+              cartTotal() === 0 ? 
+                <div>Your cart is empty</div> : 
+                <div>Your current total is:<div className="font text-4xl text-center">€{cartTotal()}</div></div>
+              } </div>
           </div>
-          <div className="w-full h-1/6 text-right align-text-bottom">
-            <Link to="/checkout" class="inline-block rounded-md border border-transparent bg-secondary py-3 px-8 text-center font-medium text-white hover:bg-secondary">Go to Checkout</Link>
+          <div className="w-full h-1/6 text-right align-bottom">{
+            cartTotal() === 0 ?  
+              <Link to="/products" class="inline-block rounded-md border border-transparent bg-secondary py-3 px-8 text-center font-medium text-white hover:bg-secondary">Go Back to Store</Link> : 
+              <Link to="/checkout" class="inline-block rounded-md border border-transparent bg-secondary py-3 px-8 text-center font-medium text-white hover:bg-secondary">Go to Checkout</Link>
+          }
           </div>
         </div>
       </div>
