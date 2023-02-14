81e786065ad5099b1baa0c399f5bf0636bff1b71
KeananA <keanan.alhouri@ucdconnect.ie>
2023-02-14 01:41:15 +0000

    Modal is finally setup

--- frontend/src/pages/CheckoutPage.jsx
+++ frontend/src/pages/CheckoutPage.jsx
@@ -8,6 +8,7 @@ import {motion} from "framer-motion"
 const CheckoutPage = () => {
     const [isFormVisible, setIsFormVisible] = useState(false);
     const [isFormVisible2, setIsFormVisible2] = useState(false);
+    const [showModal, setShowModal] = React.useState(false);
     return (
 
         
@@ -143,10 +144,112 @@ const CheckoutPage = () => {
                         
                     </div>
 
-                    <hr className="h-px my-8 bg-gray-200 border-1 dark:bg-gray-700 "/>
+                </form>
+                          
+                )}
+                <hr className="h-px my-8 bg-gray-200 border-1 dark:bg-gray-700 "/>
+
+                {/* MasterCard Button to start Modal */} 
+                <div className="flex pt-3 h-1/5 w-1/5">
+                <button type="button" onClick={() => setShowModal(true)}>
+                    <img src="https://www.dublin-insider.com/hs-fs/hubfs/visa-and-mastercard-blocked-the-cards-of-the-russian-bank-which-participated-in-the-launch-of-petro.jpg?width=860&height=520&name=visa-and-mastercard-blocked-the-cards-of-the-russian-bank-which-participated-in-the-launch-of-petro.jpg" alt="" />
+                </button>
+
+                {/* What Modal will show when clicked */} 
+                {showModal ? (
+                <>
+                {/* Parent div (position of modal) */} 
+                
+                <div className="justify-center items-center flex fixed inset-0 z-10">
+                    {/* Child div (behind the modal box) */}
+                    <div className="relative w-2/4 inset-0 rounded-lg">
+                        {/* Actual modal box */}
+                        <div className="bg-white">
+                            {/*  modal header */}
+                            <div className="border-b border-solid">
+                                <h3 className="text-2xl">Keano Test</h3>
+                            </div>
+
+                            <form className="w-full">
+                                
+
+                    <div className="flex pt-3">
+                        <div className="w-full ">
+                            <label className="block text-gray-600 text-xs" htmlFor="CardNumber">
+                                Card Number
+                            </label>
+
+                            <input className="block w-full bg-gray-200 text-gray-600" id="CardNumber" type="text" placeholder="4319*********"/>
+
+                        </div>
+
+                    </div>
+
+                    <div className="flex pt-3">
+                        <div className="w-full ">
+                            <label className="block text-gray-600 text-xs" htmlFor="CVV">
+                                CVV
+                            </label>
+
+                            <input className="block w-full bg-gray-200 text-gray-600" id="CVV" type="text" placeholder="123"/>
+
+                        </div>
+
+                    </div>
+
+                    <div className="relative pt-3 w-auto">
+                        <label className="block text-gray-600 text-xs" htmlFor="ExpiryDate">
+                            Expiry Date
+                        </label>
+
+                        <select defaultValue={"03/23"} id="ExpiryDate" className=" w-auto m-2 block bg-gray-200 text-gray-600">
+                        <option selected>Choose a country</option>
+                        <option value="0223">02/23</option>
+                        <option value="0323">03/23</option>
+                        <option value="0423">04/23</option>
+                        </select>
+                        
+                    </div>
 
                 </form>
-                )}
+
+                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 "
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Pay
+                  </button>
+                </div>
+              
+
+
+
+
+                        
+                            
+                
+                        </div>
+                    
+                    </div>
+            
+                </div>
+
+            <div className="opacity-25 fixed inset-0 bg-black"></div>
+                </>
+                ):null}
+                
+
+
+                </div>
                 
                 
             </div>
