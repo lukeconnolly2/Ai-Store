078900a31fce4f432ef986436ba7711e2fc160c5
stellakeany <stella.keany@ucdconnect.ie>
2023-03-05 17:17:47 +0000

    Creating basic context so preview of each product can be made
    Based off of ShopContext. Only temporary

--- frontend/src/pages/AdminDash.jsx
+++ frontend/src/pages/AdminDash.jsx
@@ -41,7 +41,7 @@ export default function AdminDash() {
                             <label className="block tracking-wide font-bold mb-2" forHTML="new-product-name">
                               Product Name
                             </label>
-                            <input className="appearance-none block w-full bg-gray-200 text-bgdark border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="new-product-name" type="text" placeholder="Name" />
+                            <input className="appearance-none block w-full bg-gray-200 text-bgdark border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="new-product-name" type="text" placeholder="Name" required/>
                             <p className="text-alt text-xs italic">Please fill out this field.</p>
                           </div>
                         </div>
@@ -51,14 +51,14 @@ export default function AdminDash() {
                             <label className="block tracking-wide font-bold mb-2" forHTML="new-product-price">
                               Price
                             </label>
-                            <input className="appearance-none block w-full bg-gray-200 text-bgdark border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="new-product-price" type="number" placeholder="£0.00" />
+                            <input className="appearance-none block w-full bg-gray-200 text-bgdark border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="new-product-price" type="number" placeholder="£0.00" required/>
                             <p className="text-alt text-xs italic">Please fill out this field.</p>
                           </div>
                           <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
                             <label className="block tracking-wide font-bold mb-2" forHTML="new-product-quantity">
                               Quantity
                             </label>
-                            <input className="appearance-none block w-full bg-gray-200 text-bgdark border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="new-product-quantity" type="number" placeholder="0" />
+                            <input className="appearance-none block w-full bg-gray-200 text-bgdark border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="new-product-quantity" type="number" placeholder="0" required/>
                             <p className="text-alt text-xs italic">Please fill out this field.</p>
                           </div>
                           <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
@@ -89,7 +89,7 @@ export default function AdminDash() {
                       <div className="col-span-5 md:col-span-2">
                         <div className="mb-6">
                           <label className="block tracking-wide font-bold mb-2" forHTML="new-product-image">Upload Image</label>
-                          <input className="block w-full text-bgdark border border-gray-300 rounded p-2 mb-3 cursor-pointer bg-gray-200 focus:outline-none" id="new-product-image" type="file" accept="image/*" />
+                          <input className="block w-full text-bgdark border border-gray-300 rounded p-2 mb-3 cursor-pointer bg-gray-200 focus:outline-none" id="new-product-image" type="file" accept="image/*"/>
                         </div>
                         <input type="submit" className="h-fit inline-block rounded-md border border-transparent bg-secondary py-3 px-8 text-center font-medium text-white hover:bg-secondary" value={"Submit"}/>
                       </div>
