7ea1783458713688ffe92261560d650252aa4636
KeananA <keanan.alhouri@ucdconnect.ie>
2023-03-05 01:43:13 +0000

    Cleaned indentation

--- frontend/src/pages/CheckoutPage.jsx
+++ frontend/src/pages/CheckoutPage.jsx
@@ -13,79 +13,83 @@ const CheckoutPage = () => {
 
         
         
-      <motion.main
+<motion.main
         initial={{ opacity: 0}}
         animate={{ opacity: 100 }}
         exit={{ opacity: 0 }}
         transition={{ duration: .2 }}
-      > 
+> 
       <div className="grid grid-cols-2 gap-6">
-            <div className="">
+        <div className="">
                 <h1 className=" text-center text-5xl">Checkout</h1>
                 
 
                 <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none" onClick={() => setIsFormVisible(!isFormVisible)}>Contact Information</button>
-            {isFormVisible && (
-                <form className="w-full mt-4">
+                {isFormVisible && (
+                    <form className="w-full mt-4">
 
-                <div className="flex">
-                    <div className="w-full ">
-                    <label className="block text-gray-600 text-xs" htmlFor="FirstName">
-                        FIRST NAME
-                    </label>
-                    <input
-                        className="block w-full bg-gray-200 text-gray-600"
-                        id="FirstName"
-                        type="text"
-                        placeholder="Lucas"
-                    />
+                    <div className="flex">
+                        <div className="w-full ">
+                            <label className="block text-gray-600 text-xs" htmlFor="FirstName">
+                                FIRST NAME
+                            </label>
+                            <input
+                                className="block w-full bg-gray-200 text-gray-600"
+                                id="FirstName"
+                                type="text"
+                                placeholder="Lucas"
+                            />
+                        </div>
+
+                        <div className="w-full px-3">
+                            <label className="block text-gray-600 text-xs" htmlFor="LastName">
+                                LAST NAME
+                            </label>
+                            <input
+                                className="block w-full bg-gray-200 text-gray-600"
+                                id="LastName"
+                                type="text"
+                                placeholder="Linda"
+                            />
+                        </div>
                     </div>
-                    <div className="w-full px-3">
-                    <label className="block text-gray-600 text-xs" htmlFor="LastName">
-                        LAST NAME
-                    </label>
-                    <input
-                        className="block w-full bg-gray-200 text-gray-600"
-                        id="LastName"
-                        type="text"
-                        placeholder="Linda"
-                    />
+
+                    <div className="flex pt-3">
+                        <div className="w-full ">
+                            <label className="block text-gray-600 text-xs" htmlFor="Email">
+                                EMAIL
+                            </label>
+                            <input
+                                className="block w-full bg-gray-200 text-gray-600"
+                                id="Email"
+                                type="text"
+                                placeholder="lucas@ucd.ie"
+                            />
+                        </div>
                     </div>
-                </div>
-                <div className="flex pt-3">
-                    <div className="w-full ">
-                    <label className="block text-gray-600 text-xs" htmlFor="Email">
-                        EMAIL
-                    </label>
-                    <input
-                        className="block w-full bg-gray-200 text-gray-600"
-                        id="Email"
-                        type="text"
-                        placeholder="lucas@ucd.ie"
-                    />
+
+                    <div className="flex pt-3">
+                        <div className="w-full ">
+                            <label className="block text-gray-600 text-xs" htmlFor="PhoneNumber">
+                                PHONE NUMBER
+                            </label>
+                            <input
+                                className="block w-full bg-gray-200 text-gray-600"
+                                id="PhoneNumber"
+                                type="text"
+                                placeholder="+353*********"
+                            />
+                        </div>
                     </div>
-                </div>
-                <div className="flex pt-3">
-                    <div className="w-full ">
-                    <label className="block text-gray-600 text-xs" htmlFor="PhoneNumber">
-                        PHONE NUMBER
-                    </label>
-                    <input
-                        className="block w-full bg-gray-200 text-gray-600"
-                        id="PhoneNumber"
-                        type="text"
-                        placeholder="+353*********"
-                    />
-                    </div>
-                </div>
+                    
+                    </form>
+                )}
                 
-                </form>
-        )}
-        <hr className="h-px my-8 bg-gray-200 border-1 dark:bg-gray-700 "/>
+                <hr className="h-px my-8 bg-gray-200 border-1 dark:bg-gray-700 "/>
 
 
-        <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none" onClick={() => setIsFormVisible2(!isFormVisible2)}>Shipping</button>
-      {isFormVisible2 && (
+            <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none" onClick={() => setIsFormVisible2(!isFormVisible2)}>Shipping</button>
+            {isFormVisible2 && (
                 <form className="w-full">
                 <h1>Shipping Information</h1>
                     <div className="flex">
@@ -148,10 +152,12 @@ const CheckoutPage = () => {
                 </form>
                           
                 )}
+                
                 <hr className="h-px my-8 bg-gray-200 border-1 dark:bg-gray-700 "/>
 
+                
                 {/* MasterCard Button to start Modal */} 
-                <div className="flex pt-3 h-1/5 w-1/5">
+            <div className="flex pt-3 h-1/5 w-1/5">
                 <button type="button" onClick={() => setShowModal(true)}>
                     <img src="https://www.dublin-insider.com/hs-fs/hubfs/visa-and-mastercard-blocked-the-cards-of-the-russian-bank-which-participated-in-the-launch-of-petro.jpg?width=860&height=520&name=visa-and-mastercard-blocked-the-cards-of-the-russian-bank-which-participated-in-the-launch-of-petro.jpg" alt="" />
                 </button>
@@ -172,96 +178,97 @@ const CheckoutPage = () => {
                             </div>
 
                             <form className="w-full">
-                                
+                                        
+                                <div className="flex pt-3">
+                                    <div className="w-full ">
+                                        <label className="block text-gray-600 text-xs" htmlFor="CardNumber">
+                                            Card Number
+                                        </label>
 
-                    <div className="flex pt-3">
-                        <div className="w-full ">
-                            <label className="block text-gray-600 text-xs" htmlFor="CardNumber">
-                                Card Number
-                            </label>
+                                        <input className="block w-full bg-gray-200 text-gray-600" id="CardNumber" type="text" placeholder="4319*********" required/>
 
-                            <input className="block w-full bg-gray-200 text-gray-600" id="CardNumber" type="text" placeholder="4319*********" required/>
+                                    </div>
 
-                        </div>
+                                </div>
 
-                    </div>
+                                <div className="flex pt-3">
+                                    <div className="w-full ">
+                                        <label className="block text-gray-600 text-xs" htmlFor="CVV">
+                                            CVV
+                                        </label>
 
-                    <div className="flex pt-3">
-                        <div className="w-full ">
-                            <label className="block text-gray-600 text-xs" htmlFor="CVV">
-                                CVV
-                            </label>
+                                        <input className="block w-full bg-gray-200 text-gray-600" id="CVV" type="text" placeholder="123"/>
 
-                            <input className="block w-full bg-gray-200 text-gray-600" id="CVV" type="text" placeholder="123"/>
+                                    </div>
 
-                        </div>
+                                </div>
 
-                    </div>
+                                <div className="relative pt-3 w-auto">
+                                    <label className="block text-gray-600 text-xs" htmlFor="ExpiryDate">
+                                        Expiry Date
+                                    </label>
 
-                    <div className="relative pt-3 w-auto">
-                        <label className="block text-gray-600 text-xs" htmlFor="ExpiryDate">
-                            Expiry Date
-                        </label>
+                                    <select defaultValue={"03/23"} id="ExpiryDate" className=" w-auto m-2 block bg-gray-200 text-gray-600">
+                                    <option selected>Expiry Date</option>
+                                    <option value="0223">02/23</option>
+                                    <option value="0323">03/23</option>
+                                    <option value="0423">04/23</option>
+                                    </select>
+                                    
+                                </div>
 
-                        <select defaultValue={"03/23"} id="ExpiryDate" className=" w-auto m-2 block bg-gray-200 text-gray-600">
-                        <option selected>Expiry Date</option>
-                        <option value="0223">02/23</option>
-                        <option value="0323">03/23</option>
-                        <option value="0423">04/23</option>
-                        </select>
-                        
-                    </div>
+                            </form>
+                
 
-                </form>
+                            <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                                <button
+                                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                    type="button"
+                                    onClick={() => setShowModal(false)}
+                                >
+                                    Cancel
+                                </button>
 
-                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-                  <button
-                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Cancel
-                  </button>
-                  <button
-                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 "
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Pay
-                  </button>
-                </div>
+                                <button
+                                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 "
+                                    type="button"
+                                    onClick={() => setShowModal(false)}
+                                >
+                                    Pay
+                                </button>
+                            </div>
               
 
 
 
 
                         
-                            
-                
+
                         </div>
                     
                     </div>
             
                 </div>
-
-            <div className="opacity-25 fixed inset-0 bg-black"></div>
-                </>
-                ):null}
                 
 
-
-                </div>
+                <div className="opacity-25 fixed inset-0 bg-black"></div>
+                    </>
+                    ):null}
                 
-                
-                
+    
             </div>
-            <div class="absolute left-1/2 -ml-0.5 w-0.5 h-screen bg-gray-600"></div>
-            <div className=" content-center">
-                <h1 className=" content-center">Order Summary</h1>
-
-            </div>
+                
+                
+                
         </div>
-      </motion.main>
+
+            <div class="absolute left-1/2 -ml-0.5 w-0.5 h-screen bg-gray-600"></div>
+            <div className="content-center">
+                <h1 className=" text-center">Order Summary</h1>
+
+            </div>
+    </div>
+</motion.main>
       
     )
   }
