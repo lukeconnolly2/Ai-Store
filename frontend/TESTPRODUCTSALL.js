c163fad56b6f8d9d807e274a295f378448891cf8
stellakeany <stella.keany@ucdconnect.ie>
2023-03-06 02:50:52 +0000

    Revert "Update adminproductpreview"
    This reverts commit 3339f16cac9d7df0c16d19229f6a0c67389b0ffe.

--- frontend/TESTPRODUCTSALL.js
+++ frontend/TESTPRODUCTSALL.js
@@ -5,8 +5,7 @@ export const TESTPRODUCTSALL = [
         price: 2000,
         productImgUrl: "https://media.licdn.com/dms/image/C5603AQFkcGY4lt1xow/profile-displayphoto-shrink_800_800/0/1657724853696?e=2147483647&v=beta&t=hFc3qmkNcy8DupyZXkBYJNnjkKRJfKz0Mhl9ieofVxk",
         description: "Stella is a lovely girl",
-        type: "Trained",
-        quantity: 15
+        type: "Trained"
     },
     {
         id: 2,
@@ -14,8 +13,7 @@ export const TESTPRODUCTSALL = [
         price: 2000,
         productImgUrl: "https://media.licdn.com/dms/image/C5603AQFkcGY4lt1xow/profile-displayphoto-shrink_800_800/0/1657724853696?e=2147483647&v=beta&t=hFc3qmkNcy8DupyZXkBYJNnjkKRJfKz0Mhl9ieofVxk",
         description: "Stella is a lovely girl",
-        type: "Untrained",
-        quantity: 15
+        type: "Untrained"
     },
     {
         id: 3,
@@ -23,8 +21,7 @@ export const TESTPRODUCTSALL = [
         price: 1999,
         productImgUrl: "https://media.licdn.com/dms/image/D4E03AQHhzxugjUu8Og/profile-displayphoto-shrink_400_400/0/1671472313970?e=1681344000&v=beta&t=iUedj1N7SGqjLU1Ixmf0zvi1aZ20YXrM2513fotCRiM",
         description: "Luke is a nice lad. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In scelerisque nec metus vitae efficitur. Nullam vulputate turpis quis lacus auctor, ut dapibus odio auctor. Sed at laoreet enim. Etiam feugiat mi sit amet pretium tincidunt. Fusce rhoncus nisl elit, eget rhoncus sem aliquet nec. Aliquam feugiat dui vel nulla scelerisque finibus. Morbi condimentum felis magna, at mattis erat dictum eget. Proin sed ultrices magna, ut consectetur nibh. Fusce sit amet pretium nulla. Quisque varius neque vel augue vulputate, vel ultricies odio finibus. Donec nec placerat quam. Maecenas malesuada imperdiet elit et efficitur. Pellentesque a lobortis nisi. Nunc faucibus, est sed semper vestibulum, nibh ante accumsan ligula, quis euismod sem orci at augue. Phasellus at sollicitudin magna. Duis at justo scelerisque, luctus mi ac, malesuada nunc. Nam accumsan turpis et pharetra pulvinar. Etiam rutrum velit tellus, ac hendrerit tellus porta in. Nullam a volutpat nunc, ac condimentum erat. Quisque congue non risus eget tincidunt. Sed condimentum tempus congue. Aenean elit sem, auctor sed orci at, efficitur hendrerit massa. Donec nec velit feugiat, euismod elit et, sollicitudin velit. Curabitur elit purus, dignissim nec magna at, consectetur venenatis mauris. Sed sed porttitor diam. Morbi et massa nec dui consectetur cursus quis quis felis. Aenean et faucibus felis, quis ullamcorper nulla.",
-        type: "Trained",
-        quantity: 15
+        type: "Trained"
     },
     {
         id: 4,
@@ -32,8 +29,7 @@ export const TESTPRODUCTSALL = [
         price: 1999,
         productImgUrl: "https://media.licdn.com/dms/image/D4E03AQHhzxugjUu8Og/profile-displayphoto-shrink_400_400/0/1671472313970?e=1681344000&v=beta&t=iUedj1N7SGqjLU1Ixmf0zvi1aZ20YXrM2513fotCRiM",
         description: "Luke is a nice lad. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In scelerisque nec metus vitae efficitur. Nullam vulputate turpis quis lacus auctor, ut dapibus odio auctor. Sed at laoreet enim. Etiam feugiat mi sit amet pretium tincidunt. Fusce rhoncus nisl elit, eget rhoncus sem aliquet nec. Aliquam feugiat dui vel nulla scelerisque finibus. Morbi condimentum felis magna, at mattis erat dictum eget. Proin sed ultrices magna, ut consectetur nibh. Fusce sit amet pretium nulla. Quisque varius neque vel augue vulputate, vel ultricies odio finibus. Donec nec placerat quam. Maecenas malesuada imperdiet elit et efficitur. Pellentesque a lobortis nisi. Nunc faucibus, est sed semper vestibulum, nibh ante accumsan ligula, quis euismod sem orci at augue. Phasellus at sollicitudin magna. Duis at justo scelerisque, luctus mi ac, malesuada nunc. Nam accumsan turpis et pharetra pulvinar. Etiam rutrum velit tellus, ac hendrerit tellus porta in. Nullam a volutpat nunc, ac condimentum erat. Quisque congue non risus eget tincidunt. Sed condimentum tempus congue. Aenean elit sem, auctor sed orci at, efficitur hendrerit massa. Donec nec velit feugiat, euismod elit et, sollicitudin velit. Curabitur elit purus, dignissim nec magna at, consectetur venenatis mauris. Sed sed porttitor diam. Morbi et massa nec dui consectetur cursus quis quis felis. Aenean et faucibus felis, quis ullamcorper nulla.",
-        type: "Untrained",
-        quantity: 15
+        type: "Untrained"
     },
     {
         id: 5,
@@ -41,8 +37,7 @@ export const TESTPRODUCTSALL = [
         price: 1,
         productImgUrl: "https://media.licdn.com/dms/image/C4E03AQEbNCzOZcp4ow/profile-displayphoto-shrink_400_400/0/1663964055500?e=1681344000&v=beta&t=2dzmJC0tTCWbA2BZ28FYbiZz-FJUoK2g9CcZnbfFTR4",
         description: "Keanos a nice lad",
-        type: "Trained",
-        quantity: 15
+        type: "Trained"
     },
     {
         id: 6,
@@ -50,7 +45,6 @@ export const TESTPRODUCTSALL = [
         price: 1,
         productImgUrl: "https://media.licdn.com/dms/image/C4E03AQEbNCzOZcp4ow/profile-displayphoto-shrink_400_400/0/1663964055500?e=1681344000&v=beta&t=2dzmJC0tTCWbA2BZ28FYbiZz-FJUoK2g9CcZnbfFTR4",
         description: "Keanos a nice lad",
-        type: "Untrained",
-        quantity: 15
+        type: "Untrained"
     },
 ]
