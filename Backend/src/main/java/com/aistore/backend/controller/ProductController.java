4c704359737293b2d5053e440c129a2a717d17ac
Luke Connolly <luke.connolly2@ucdconnect.ie>
2023-03-09 19:50:04 +0000

    In Memory test DB working

--- Backend/src/main/java/com/aistore/backend/controller/ProductController.java
+++ Backend/src/main/java/com/aistore/backend/controller/ProductController.java
@@ -6,6 +6,7 @@ import com.aistore.backend.dao.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,7 +17,7 @@ public class ProductController {
     private ProductRepository repository;
 
     @PostMapping("/addProduct")
-    public String addProduct(Product product){
+    public String addProduct(@RequestBody Product product){
         repository.save(product);
         return "Product Saved";
     }
