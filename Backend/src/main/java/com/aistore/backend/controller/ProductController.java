bcd2a9f3af009ff405874c5378f8877c1e45f3b5
Luke Connolly <luke.connolly2@ucdconnect.ie>
2023-03-09 20:04:58 +0000

    Added ability to delete product

--- Backend/src/main/java/com/aistore/backend/controller/ProductController.java
+++ Backend/src/main/java/com/aistore/backend/controller/ProductController.java
@@ -4,12 +4,11 @@ package com.aistore.backend.controller;
 import com.aistore.backend.Model.Product;
 import com.aistore.backend.dao.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cache.support.NullValue;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ProductController {
@@ -25,5 +24,20 @@ public class ProductController {
     @GetMapping("/getProducts")
     public List<Product> getProducts() {
         return repository.findAll();
+    }
+
+    @GetMapping("/getProduct/{id}")
+    public Optional<Product> getProductById(@PathVariable String id){
+        return repository.findById(Integer.parseInt(id));
+    }
+
+    @PostMapping("/deleteProduct")
+    public String deleteProduct(@RequestBody String id){
+        Optional<Product> p = repository.findById(Integer.parseInt(id));
+        if(p.isEmpty()){
+            return "Product doesnt exits";
+        }
+        repository.delete(p.get());
+        return "Deleted";
     }
 }
