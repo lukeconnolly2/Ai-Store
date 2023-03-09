ffc16d0933f6152db7899342fcf78d5f7a80b4b0
Luke Connolly <luke.connolly2@ucdconnect.ie>
2023-03-09 19:41:43 +0000

    Added h2 database mappings

--- Backend/src/main/java/com/aistore/backend/controller/ProductController.java
+++ Backend/src/main/java/com/aistore/backend/controller/ProductController.java
@@ -1,9 +1,28 @@
 package com.aistore.backend.controller;
 
 
+import com.aistore.backend.Model.Product;
+import com.aistore.backend.dao.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ProductController {
+    @Autowired
+    private ProductRepository repository;
 
+    @PostMapping("/addProduct")
+    public String addProduct(Product product){
+        repository.save(product);
+        return "Product Saved";
+    }
+
+    @GetMapping("/getProducts")
+    public List<Product> getProducts() {
+        return repository.findAll();
+    }
 }
