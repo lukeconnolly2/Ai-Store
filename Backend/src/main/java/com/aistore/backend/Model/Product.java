589e19a9044a32bd76243230963e200cc54ad252
Luke Connolly <luke.connolly2@ucdconnect.ie>
2023-03-15 23:42:29 +0000

    Changes

--- Backend/src/main/java/com/aistore/backend/Model/Product.java
+++ Backend/src/main/java/com/aistore/backend/Model/Product.java
@@ -1,13 +1,13 @@
 package com.aistore.backend.Model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
