e2c5876f5507c994b9715f927f43d884daa11e94
stellakeany <stella.keany@ucdconnect.ie>
2023-03-15 20:39:08 +0000

    Creating new user

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -24,7 +24,7 @@ export default function Login() {
       auth.login(username)
       navigate("/products")
     }else{
-      alert(response.type)
+      console.log(response)
     }
   }
 
--- frontend/src/pages/NewProfile.jsx
+++ frontend/src/pages/NewProfile.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from 'react'
+import { signupUser } from '../helpers/signupUser'
+import { Navigate, useNavigate } from 'react-router-dom'
 
 export default function NewProfile() {
-    const [message, setMessage] = useState('')
+    const navigate = useNavigate()
+    const [username, setUser] = useState('')  
+    const [password, setPassword] = useState('')  
 
     const handleLogin = async (e) => {
         e.preventDefault()
@@ -9,15 +13,14 @@ export default function NewProfile() {
         username,
         password
         })
-        const response = await loginUser({
+        const response = await signupUser({
         username,
         password
         });
-        if (response.accessToken === username) {
-        auth.login(username)
-        navigate("/products")
-        }else{
-        alert(response.type)
+        if (response.type === 'Saved'){
+            navigate("/login")
+        } else{
+            alert("Something went wrong!")
         }
     }
 
@@ -30,18 +33,17 @@ export default function NewProfile() {
                     
                         <div className="mb-4">
                         <label className="block font-bold mb-2" htmlFor="new-username" >Username</label>
-                        <input className="border border-gray-600 p-2 w-full rounded-lg text-bgdark"  placeholder='Username' type="text" id="username" onChange={(e) => setUser(e.target.value)}  required/>
+                        <input className="border border-gray-600 p-2 w-full rounded-lg text-bgdark"  placeholder='Username' type="text" id="new-username" onChange={(e) => setUser(e.target.value)}  required/>
                         </div>
 
                         <div className="mb-4">
                         <label className="block font-bold mb-2" htmlFor="new-password">Password</label>
-                        <input className="border border-gray-600 p-2 w-full rounded-lg text-bgdark"  placeholder='Password' type="password" id="password" onChange={(e) => setPassword(e.target.value)} required/>
+                        <input className="border border-gray-600 p-2 w-full rounded-lg text-bgdark"  placeholder='Password' type="password" id="new-password" onChange={(e) => setPassword(e.target.value)} required/>
                         </div>
 
                         <div className="mb-4">
                         <button className="bg-bgdark hover:bg-bglight text-white font-bold p-3 w-full rounded-lg" onClick={handleLogin}>Create</button>
                         </div>
-                        {message !== '' ? <h3>{message}</h3> : ''}
                     </div>
                 </div>
             </div>
